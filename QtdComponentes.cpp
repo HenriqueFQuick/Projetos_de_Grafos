17fa15f34bdef9ed7c4c82a7182d3b31b8c9b200
HenriqueFQuick <henrique.fquick@gmail.com>
2019-09-10 23:15:04 -0300

    Pequenas alteracoes de comentarios

--- QtdComponentes.cpp
+++ QtdComponentes.cpp
@@ -6,10 +6,12 @@
 *Funcionamento:
 *    Busca por profundidade adaptado:
 *        Todos os vertices sao brancos, quando o algoritmo passa pelo vertice, ele se torna cinza
-*        e comeca a passar em todos os vertices adjacentes a ele, quando todos forem verificados
-*        este, se torna preto. o metodo continua ate que todos os vertices de um componente forem
+*        e comeca a passar em todos os vertices adjacentes a ele, quando todos forem verificados,
+*        este se torna preto. o metodo continua ate que todos os vertices de um componente forem
 *        pretos, ou seja, se ele sair do metodo e tornar a voltar, significa que existe mais de um
 *        componente.
+*Entrada:
+*   Numero de grafos, numero de vertices, numero de arestas, 1 linha para cada aresta, indicando qual aresta eh ("a b"  -> aresta de a para b // b para a)
 */
 #include <iostream>
 using namespace std;
@@ -22,7 +24,7 @@ using namespace std;
 *   vertice = vetor de cor de cada vertice
 *   tamanho  = tamanho da matriz 
 */
-void percorreMatriz(int v, int **matriz, int vertice[], int tamanho){
+void percorreMatriz(int v, int **matriz, int *vertice, int tamanho){
 
     vertice[v] = 1;                                             //a cor do vertice atual passa a ser cinza
     cout << (char)(v+97) << ",";                                //printa o vertice (o + 97 eh pela tabela ascii)
@@ -49,7 +51,7 @@ int** criaMatriz(int tamanho){
             matriz[i] = new int[tamanho];
         }
 
-        for(int i = 0; i < tamanho; i++){           //inicializacao da matriz
+        for(int i = 0; i < tamanho; i++){           //inicializacao da matriz com 0
             for(int j = 0; j < tamanho; j++){
                 matriz[i][j] = 0;
             }
@@ -73,8 +75,8 @@ int* criaVetor(int tamanho){
 
 
 /*
-*Metodo para printar a matriz de adjacencias ( nao eh utilizado no codigo )
-*Se quiser utiliza-lo, eh so colocar na main, depois de acrescentadas as arestas na matriz
+*Metodo para printar a matriz de adjacencias ( nao eh utilizado no codigo )( apenas para visualizacao )
+*Se quiser utiliza-lo, eh so colocar na main, depois de acrescentadas as arestas na matriz ( linha 140 nesse codigo )
 *Parametros:
 *   matriz = a matriz de adjacencias desejada
 *   tamanho = tamanho da matriz
@@ -105,7 +107,7 @@ void acrescentaArestas(int** matriz, int e){
             char x , y;                         //para cada aresta, le os dois vertices
             cin >> x;                           //e acrescenta na matriz de adjacencias
             cin >> y;
-                                                //como os grafos lidos nao sao orientados, acrescenta-se as arestas nos dois sentidos
+                                                //como os grafos lidos sao grafos nao orientados, acrescenta-se as arestas nos dois sentidos
             matriz[x - 97][y - 97] = 1;         //acrescenta x -> y
             matriz[y - 97][x - 97] = 1;         //acrescenta y -> x
 
@@ -130,13 +132,11 @@ int main(){
         int e;                                  //e a quantidade de arestas existentes
         cin >> e;
 
-        int** matriz = criaMatriz(tamanho);     //Criacao e inicializacao da matriz
+        int** matriz = criaMatriz(tamanho);     //criacao e inicializacao da matriz
 
         int* vertice = criaVetor(tamanho);      //criacao e inicializacao do vetor
 
         acrescentaArestas(matriz, e);           //acrescenta as arestas na matriz de adjacencias
-
-        //printMatriz(matriz, tamanho);         //chamada para printa a matriz de adjacencias (extra, apenas visualizacao)
 
         cout << "Case " << c << "#:\n";
 
